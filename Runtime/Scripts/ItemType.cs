d8fb5c46277c10afe9331c3bd77ea9c58d0dc37b
wagskydive <wagskydive@gmail.com>
2022-10-31 01:53:55 +0100

    Editor window design

--- Runtime/Scripts/ItemType.cs
+++ Runtime/Scripts/ItemType.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 
 namespace InventoryPackage
 {
-    [Serializable]
-    public class ItemType
+    public class ItemType : IItemType
     {
-        public string TypeName {get => typeName; }
+        public string TypeName { get => typeName; }
         [SerializeField] private readonly string typeName;
 
-        public string Description {get =>  description; }
-        [SerializeField] private readonly string  description;
+        public string Description { get => description; }
+        [SerializeField] private string description;
 
-        public int StackSize {get => stackSize;}
-        [SerializeField] private readonly int stackSize;
+        public int StackSize { get => stackSize; }
+        [SerializeField] private int stackSize;
+
+        public Texture2D Icon { get => icon; }
+
+        private Texture2D icon;
 
 
         public ItemType(string typeName, int stackSize = 100, string discription = "no description written.")
@@ -23,11 +26,29 @@ namespace InventoryPackage
             this.stackSize = stackSize;
         }
 
+        internal void SetDescription(string description)
+        {
+            this.description = description;
+        }
+
+        internal void SetStackSize(int stackSize)
+        {
+            this.stackSize = stackSize;
+        }
+
+        internal void SetIcon(Texture2D icon)
+        {
+            this.icon = icon;
+        }
+
+
 
 
         public static ItemType Empty()
         {
             return new ItemType("Empty", 1);
         }
+
+
     }
 }
