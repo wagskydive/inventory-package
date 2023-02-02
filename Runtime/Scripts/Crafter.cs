316cd079cf80a02eadb9113acdbcd29db04e0bd2
wagskydive <willemdegroot.tv@gmail.com>
2023-02-02 11:50:44 +0100

    Added "Empty" check

--- Runtime/Scripts/Crafter.cs
+++ Runtime/Scripts/Crafter.cs
@@ -25,9 +25,13 @@ namespace InventoryPackage
 
         public static bool CanCraft(Recipe recipe, Inventory input, Inventory output, ItemInstance tool)
         {
-            if(recipe.ToolType != null &&  recipe.ToolType != tool.TypeOfItem)
+            if(recipe.ToolType != null && recipe.ToolType.TypeName != "Empty")
             {
-                return false;
+                if(tool == null || recipe.ToolType != tool.TypeOfItem)
+                {
+                    return false;
+                }
+                
             }
             for (int i = 0; i < recipe.Ingredients.Slots.Length; i++)
             {
